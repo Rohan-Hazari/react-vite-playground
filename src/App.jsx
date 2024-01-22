7b000507a4668227d88f2e7f2785c25b9f3eb13f
Rohan Hazari <rohanhazari2012@gmail.com>
2024-01-23 02:01:25 +0530

    Starter commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import Form from "./component/Form";
 import Palette from "./component/Palette";
+import Values from "values.js";
+import { useState } from "react";
+
 function App() {
+  const [colors, setColors] = useState(new Values("#ffffff").all(10));
+
+  const addColor = (color) => {
+    const newColors = new Values(color).all(10);
+    setColors(newColors);
+    console.log(newColors);
+  };
+
   return (
     <main>
-      <Form />
+      <Form addColor={addColor} />
       <Palette />
     </main>
   );
--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
-const Form = () => {
+const Form = ({ addColor }) => {
   const [color, setColor] = useState("");
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addColor(color);
+  };
   return (
     <section className="container">
       <h4>Palette Generator</h4>
       <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => {
+            setColor(e.target.value);
+          }}
+        />
         <input
           type="text"
           value={color}
