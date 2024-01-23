7d2237771d1974ee012c8bba92acc72017e52b37
Rohan Hazari <rohanhazari2012@gmail.com>
2024-01-23 14:05:23 +0530

    Boilerplate code

--- src/component/Palette.jsx
+++ src/component/Palette.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import SingleColor from "./SingleColor";
 
 const Palette = () => {
-  return <section>Palette</section>;
+  return <section className="colors">Palette</section>;
 };
 
 export default Palette;
